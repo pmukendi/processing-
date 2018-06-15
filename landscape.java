0253a2647cdd76d9718f062870590c1ceb2b6510
phil <36393437+pmukendi@users.noreply.github.com>
2018-06-15 05:26:15 -0800

    Update landscape.java

--- landscape.java
+++ landscape.java
@@ -1,77 +1,67 @@
-class Fish {
-  float x = random(-100, width);
-  float y = random(493, 547) ;
-  float xspeed = random(1, 3);
-  void swim() {
-    x = x + xspeed;
+int i;                 //declare variable 
+int a; 
+int b; 
+int c; 
+int d; 
+int e; 
 
-    if (x > width) {
-      x = random(-200);
+class Gun {                        // making bullets
+  float X = mouseX+ 15;
+  float Y = 48;
+  float Yspeed = random(2, 4);
+  void shoot() { 
+    Y =  Y + Yspeed;
+    if (Y > height) {
+      Y = 48;
     }
   }
 
-  void show() { 
-
-    fill(#919B9A);
-    stroke(#787C7C);
+  void show() {
+    fill(#343232);
+    stroke(#0D0606);
     strokeWeight(4);
-    line(x, y, x+7, y);
+    line(mouseX, Y, mouseX, Y+9);
   }
 }
 
-
-Fish[] fishies = new Fish[10];
+Gun[] bullet = new Gun[1];
 
 void setup() {
-  size(600, 600);
-  for (int i = 0; i < fishies.length; i++) {
-    fishies[i] = new Fish();
+  size(600, 770); // resolution 
+
+  for (int i = mouseX; i < bullet.length; i++) {
+    bullet[i] = new Gun();
   }
-}
+} 
 
 void draw() {
-  background(#39E346);
-  fill(#42F2E8);
-  noStroke();
-  rect(0, 0, 600, 400);
-  fill(#C68636);
-  stroke(#985A28);
-  strokeWeight(4);
-  rect(400, 300, 32, 110, 3); //tree
-  stroke(0);
-  rect(100, 370, 90, 90); //house
-  stroke(0, 0, 0);
-  rect(170, 310, 20, 37); //chimney
-  triangle(80, 370, 147, 290, 210, 370); //roof
-  noStroke();
-  fill(#39E346);
-  stroke(#299837);
-  ellipse(415, 286, 110, 90); //leaves
-  fill(#39E346);
-  noStroke();
-  rect(398, 407, 50, 18); //roots
-  stroke(0);
-  strokeWeight(1);
-  fill(255, 255, 255);
-  rect(123, 391, 30, 30); //window
-  line(138, 392, 138, 421);
-  line(123, 407, 152, 407); //frame
-  noStroke();
-  fill(200, 200, 200);
-  ellipse(180, 280, 30, 30);
-  ellipse(186, 270, 20, 20);
-  ellipse(179, 250, 18, 20);
-  ellipse(183, 227, 10, 10); //smoke
-  stroke(#FFCE1A);
-  fill(#F1FF34);
-  ellipse(570, 30, 100, 100);//sun
-  noStroke();
-  fill(#4BCBF7);
-  rect(0, 490, width, 60); //river
+  if (mousePressed == true) { 
+    noCursor();
+  } else {
+    cursor (HAND);
+  }
 
-  for (int i = 0; i < fishies.length; i++) {  
-    fishies[i].swim();
-    fishies[i].show();                // fish
-  }  
-  println(mouseX, mouseY);
+  background(#1AD82E);   //background color/screen
+  fill(#7A7C7B);
+  noStroke();
+  rect(0, 0, 600, 55);            //bad guy floor 
+  rect(0, 715, 600, 55);        //hero guy floor 
+  stroke(0); 
+  fill(#F20202);
+  ellipse(mouseX, 740, 40, 40);            //hero guy
+  noFill();
+  fill(#835714);
+  ellipse(mouseX, 40, 40, 40);         //bad guy 
+  fill(#242926);
+  rect(mouseX+15, 40, 10, 20);         //bad guy's gun
+  fill(#0238F5);
+  ellipse(200, 750, 20, 20);       //pedestrian
+  ellipse(500, 760, 20, 20);       //pedestrian 2
+  ellipse(50, 763, 20, 20);       //pedestrian 3
+  ellipse(580, 750, 20, 20);       //pedestrian 4
+
+  for (int i = 0; i < bullet.length; i++) {
+    bullet[i].shoot();
+    bullet[i].show();
+  }
 }
